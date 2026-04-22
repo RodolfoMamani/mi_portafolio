2b39e8734ca44774ddbd45ae12d579318b98b506
RodolfoMamani <rodomm3@gmail.com>
2026-04-22 02:42:57 -0400

    Swagger get api usuarios

--- api-gestion/src/routes/usuario.routes.js
+++ api-gestion/src/routes/usuario.routes.js
@@ -1,6 +1,30 @@
 const routes = require('express').Router();
 const controllerUsuario = require('../controllers/usuario.controller');
 
+
+/**
+ * @swagger
+ * /api/usuarios:
+ *  get:
+ *    summary: Obtener una lista de usuarios
+ *    tags: [Usuario]
+ *    responses:
+ *      200:
+ *        description: Lista de usuarios exitosamente
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                $ref: '#/components/schemas/Usuario'
+ *                  
+ *  
+ */
+
+
+
+
 routes.get('/usuarios', controllerUsuario.getUsuarios);
 
 module.exports = routes;
+
